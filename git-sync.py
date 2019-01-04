55f1d7f3d6264c29fe7d171a490d08b8d227a9e7
Sven Roederer <devel-sven@geroedel.de>
2019-01-04 23:56:14 +0100

    initial commit of working code
    still very hackish

---
+++ git-sync.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+
+# https://gitpython.readthedocs.io/en/stable/tutorial.html#tutorial-label
+
+from git import Repo, Remote
+import os
+
+def syncrepo(workdir, user, passw, src, dst, srcbranch, dstbranch):
+    #https://stackoverflow.com/questions/44784828/gitpython-git-authentication-using-user-and-password?noredirect=1&lq=1
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    os.environ['GIT_ASKPASS'] = os.path.join(project_dir, 'askpass.py')
+
+    repo = Repo(os.path.join(REPODIR, workdir))
+    assert not repo.bare
+    print (repo.remotes)
+    remotefrom=repo.remote(name=src)
+    remoteto=repo.remote(name=dst)
+    fetchinfo=remotefrom.fetch()
+#    print(fetchinfo.ref)
+    if dstbranch in repo.heads:
+        print("branch '%s' exists" % dstbranch)
+    else:
+        print("branch '%s' needs to be created" % dstbranch)
+        repo.create_head(dstbranch, srcbranch)
+#    workbranch = repo.heads[srcbranch].checkout(force=True)
+#    print(workbranch)
+#    workbranch.set_commit(srcbranch).commit
+#    repo.head.reference=srcbranch
+    repo.git.checkout(dstbranch)
+    repo.head.reset(src+"/"+srcbranch, index=True, working_tree=True)
+#    workbranch.reset()
+#    repo.heads[srcbranch].HEAD.reset()
+#    print(repo.heads["sync"])
+    remotefrom.pull(refspec=srcbranch)
+    remoteto.push(refspec=dstbranch)
+
+
+REPOURL="https://github.com/freifunk-berlin/openwrt-core.git"
+REPODIR="/tmp/gitsync"
+REPOFROM="upstream"
+REPOTO="origin"
+
+REPOS = {
+    "https://github.com/freifunk-berlin/openwrt-core.git" : {
+	"user" : "SAm0815",
+	"pass" : "test",
+	"branches" : {
+	    "master" : "master",
+	    "lede-17.01" : "lede-17.01",
+	        },
+	    },
+    "https://github.com/freifunk-berlin/openwrt-luci.git" : {
+	"user" : "SAm0815",
+	"pass" : "test",
+	"branches" : {
+	    "master" : "master",
+		},
+	    },
+        }
+for repourl, repodata in REPOS.items():
+#    print(repourl, repodata)
+    print(repourl)
+    print(repodata["user"])
+    print(repodata["pass"])
+    print(repodata["branches"])
+    syncbranches=repodata["branches"]
+    for srcbranch, dstbranch in syncbranches.items():
+#        print(srcbranch)
+        print('sync %s to %s' % (srcbranch, dstbranch))
+
+REPOLIST = [
+    {
+#    "srcrepo" : "https://github.com/openwrt/openwrt.git",
+#    "dstrepo" : "https://github.com/freifunk-berlin/openwrt-core.git",
+    "srcremote" : "upstream",
+    "dstremote" : "origin",
+    "workdir" : "openwrt", 
+    "user" : "SAm0815",
+    "pass" : "test",
+    "branches" : {
+        "master" : "master",
+        "lede-17.01" : "lede-17.01",
+        },
+    }, {
+#    "srcrepo" : "https://github.com/openwrt/luci.git",
+#    "dstrepo" : "https://github.com/freifunk-berlin/openwrt-luci.git",
+    "srcremote" : "upstream",
+    "dstremote" : "origin",
+    "workdir" : "luci", 
+    "user" : "SAm0815",
+    "pass" : "test",
+    "branches" : {
+        "master" : "master",
+        },
+    },
+]
+for repodata in REPOLIST:
+    print(repodata)
+#    print(repodata["srcrepo"])
+#    print(repodata["dstrepo"])
+    print(repodata["user"])
+    print(repodata["pass"])
+    print(repodata["branches"])
+    syncbranches=repodata["branches"]
+    for srcbranch, dstbranch in syncbranches.items():
+        print('sync %s to %s' % (srcbranch, dstbranch))
+#        print('sync %s/%s to %s/%s as user %s' % (repodata["srcrepo"], srcbranch, repodata["dstrepo"], dstbranch, repodata["user"]) )
+        syncrepo( repodata["workdir"], repodata["user"], repodata["pass"], repodata["srcremote"], repodata["dstremote"], srcbranch, dstbranch)
+
+exit(0)
+
+#https://stackoverflow.com/questions/44784828/gitpython-git-authentication-using-user-and-password?noredirect=1&lq=1
+project_dir = os.path.dirname(os.path.abspath(__file__))
+os.environ['GIT_ASKPASS'] = os.path.join(project_dir, 'askpass.py')
+#os.environ['GIT_USERNAME'] = username
+#os.environ['GIT_PASSWORD'] = getpass()
+
+
+#repo = Repo(self.rorepo.working_tree_dir)
+#assert not repo.bare
+#repo = Repo.init(os.path.join(REPODIR, 'bare-repo'), bare=True)
+#assert repo.bare
+
+#repo = Repo.clone_from(REPOURL, REPODIR)
+repo = Repo(os.path.join(REPODIR, 'openwrt'))
+assert not repo.bare
+print (repo.remotes)
+remotefrom=repo.remote(name=REPOFROM)
+remoteto=repo.remote(name=REPOTO)
+#remotefrom.fetch(refspec=REPOFROM)
+remotefrom.fetch()
+remotefrom.pull(refspec="master")
+remoteto.push(refspec="master")
